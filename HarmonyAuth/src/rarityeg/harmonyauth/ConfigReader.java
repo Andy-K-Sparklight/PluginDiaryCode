0f7c850ecf71f5a44d4d0f235fdbcf3a006bca27
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-02-05 22:29:33 +0800

    fix: HarmonyAuth unnecessary code

--- HarmonyAuth/src/rarityeg/harmonyauth/ConfigReader.java
+++ HarmonyAuth/src/rarityeg/harmonyauth/ConfigReader.java
@@ -3,16 +3,14 @@ package rarityeg.harmonyauth;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public final class ConfigReader {
+    public static FileConfiguration config = HarmonyAuth.instance.getConfig();
+
     public static boolean isPlayerRegistered(String playerName) {
-        FileConfiguration config = HarmonyAuth.instance.getConfig();
+
         return config.contains(playerName.toLowerCase());
     }
 
     public static boolean verifyPassword(String playerName, String password) {
-        if (!isPlayerRegistered(playerName)) {
-            return false;
-        }
-        FileConfiguration config = HarmonyAuth.instance.getConfig();
         return password.equals(config.getString(playerName.toLowerCase()));
     }
 
